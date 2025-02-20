c5d68ddec61a93a6da05ab3470f63b4a4077a1af
ST10301110 <st10301110@vcconnect.edu.za>
2025-02-20 18:03:02 +0200

    Part 5 database done

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
+using MvcMovie.Models;
 namespace MvcMovie
 {
     public class Program
@@ -16,13 +17,20 @@ namespace MvcMovie
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            using (var scope = app.Services.CreateScope())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                var services = scope.ServiceProvider;
+
+                SeedData.Initialize(services);
             }
+
+                // Configure the HTTP request pipeline.
+                if (!app.Environment.IsDevelopment())
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
 
             app.UseHttpsRedirection();
             app.UseRouting();
